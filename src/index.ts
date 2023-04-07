793d43f9d572e185a5bbf7bc7c6ac6e7562100fb
kevs <kevin.quintana.256bit@gmail.com>
2023-04-07 16:33:08 -0500

    base de api

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+//import reportsRouter from './router/reports.router.js';
+
+class Server{
+
+    private app: express.Express
+    
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+        this.start();
+    }
+
+    private config(){
+        dotenv.config();
+        this.app.use(cors({
+            origin: process.env.CLIENT_HOST! || '*',
+            credentials: true
+        }));
+        this.app.use(express.json());
+        this.app.use(morgan('dev'));
+    }
+
+    private routes(){
+        //this.app.use(cardsRouter.router);
+
+    }
+
+    private start(){
+        this.app.listen(parseInt(process.env.API_PORT!), process.env.API_HOST!, ()=>{
+            console.log(`Listen on http://${process.env.API_HOST}:${process.env.API_PORT}/`);
+        });
+    }
+}
+
+new Server();
